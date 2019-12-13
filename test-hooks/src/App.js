0693650877f33f8b1692ad0de8a24433d72f43d2
Gabriel Ortega <gabrielo_9103@hotmail.com>
2019-12-13 18:35:58 -0500

    Counter added

--- test-hooks/src/App.js
+++ test-hooks/src/App.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Container } from 'react-bootstrap'
+import { Card, Container, Form, Button } from 'react-bootstrap'
 function App() {
+
+  const [name, setName] = useState('default')
+  const [counter, setCounter] = useState(1)
+
+  const clickButton = (e) => {
+    setCounter(counter+1)
+  }
+
   return (
-    <div className="App">
+    <div>
       <Container>
         <Card border="success" style={{ margin: '1rem' }}>
+        <Card.Header>{name}: {counter}</Card.Header>
           <Card.Body>This is some text within a card body.</Card.Body>
         </Card>
-        <Card border="success" style={{ margin: '1rem' }}>
-          <Card.Body>This is some text within a card body.</Card.Body>
-        </Card>
-        <Card border="success" style={{ margin: '1rem' }}>
-          <Card.Body>This is some text within a card body.</Card.Body>
-        </Card>
+        <Container>
+          <Form>
+            <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="input"
+                  placeholder="Enter your Name"
+                  value = {name}
+                  onChange={(e) => {
+                    setName(e.target.value)
+                  }}
+                />
+            </Form.Group>
+            <Button variant="primary" size="lg" active 
+              onClick = {clickButton}
+              >
+              Primary button
+            </Button>
+          </Form>
+        </Container>
+        
       </Container>
-          
     </div>
   );
 }
